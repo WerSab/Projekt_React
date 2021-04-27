81245658b63fc6142fb20b0e5123bd780223f34c
Your Name <you@example.com>
2021-04-27 11:51:05 +0200

    Przelanczanie trybu edycji

--- src/pages/Playlists.tsx
+++ src/pages/Playlists.tsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Details } from '../components/Details'
 import { Form } from '../components/Form'
 import { List } from '../components/List'
-
+//color: playlist.public ? "red" : "blue",
 interface Props {
-    
+
 }
 
+
 export const Playlists = (props: Props) => {
+    const [TrybEdycji, setTrybEdycji] = useState(false)
+
     return (
         <div>
-           <h2>Playlist</h2> 
-           <div className="row">
-               <div className="col">Lista
-               <List/>
-               </div>
-               <div className="col">Szczegóły
-               <Details/>
-               <Form/>
-               </div>
-               
-           </div>
+            <h2>Playlist</h2>
+            <div className="row">
+                <div className="col">Lista
+               <List />
+                </div>
+                <div className="col">Szczegóły
+               {TrybEdycji ? 
+                    <div>
+                        <Form />
+                        <button className="btn btn-danger" onClick={e => { setTrybEdycji(false) }}>Cancel</button>
+                        <button className="btn btn-success">Save</button>
+                    </div> : <div>
+                        <Details />
+                        <button className="btn btn-info" onClick={e => { setTrybEdycji(!TrybEdycji) }}>Edit</button>
+                    </div>}
+
+
+                </div>
+
+
+            </div>
         </div>
     )
 }
